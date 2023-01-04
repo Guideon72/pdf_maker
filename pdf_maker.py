8bc96cb87359a781813d152f2baa63f2214f7503
Eric Snyder <guideon72@hotmail.com>
2023-01-04 10:23:45 -0800

    Use for loop to add lines to each doc page in pdf_maker.py

--- pdf_maker.py
+++ pdf_maker.py
@@ -14,23 +14,16 @@ def mk_page(in_txt):
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=in_txt, align="L", ln=1, border=0)
-    pdf.line(10, 21, 200, 21)
+
+    for i in range(20, 297, 10):
+        pdf.line(10, i, 200, i)
 
     # building Footer (should do this for real by extending FPD and adding Footer() method)
-    pdf.ln(260)
+    pdf.ln(267)
     pdf.set_font(family="Times", style="I", size=10)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=8, txt=in_txt, align="R")
 
-
-# mk_page("My first Python PDF")
-# pdf.set_font(family="Times", style="", size=10)
-
-# mk_page("My second line of text")
-# # pdf.add_page()
-
-# # pdf.cell(w=0, h=12, txt="My first Python PDF", align="L", ln=1, border=1)
-# # pdf.cell(w=0, h=12, txt="My second line of text", align="L", ln=1, border=0)
 
 for i, row in doc_df.iterrows():
     for i in range(row["Pages"]):
